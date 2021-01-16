b0d66fa6e46cdd686eae9bfd2afc117f20b7f7fd
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-01-16 08:52:05 +0700

    Update dark mode configuration

--- js/theme.js
+++ js/theme.js
@@ -2,7 +2,7 @@ const Darker = require("darkreader");
 
 Darker.setFetchMethod(window.fetch);
 
-const EnableDarkMode = () => void Darker.enable();
+const EnableDarkMode = () => void Darker.enable({ brightness: 100 + 10 });
 const DisableDarkMode = () => void Darker.disable();
 
 module.exports = {
